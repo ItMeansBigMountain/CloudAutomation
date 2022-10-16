276e87bbc96d1806d987bf4f12412c4152e342a5
itMeansBigMountain <affan.fareed@gmail.com>
2022-10-16 13:05:01 -0500

    changed TF version

--- TerraForm/Templates/deployResourceGroup-output.tf
+++ TerraForm/Templates/deployResourceGroup-output.tf
@@ -1,8 +1,12 @@
-# RESOURCE TAG FROM main.tf
+# ***********************************
+# RENAME FILE TO output.tf
+# ***********************************
+
+
+# DISPLAY SPECIFIC DATA FROM RESOURCE TAG IN main.tf
 output "resource_group_name" {
   value = azurerm_resource_group.example_rg.name
 }
 output "resource_group_name" {
   value = azurerm_resource_group.example_rg2.name
 }
-
--- TerraForm/Templates/deployResourceGroup.tf
+++ TerraForm/Templates/deployResourceGroup.tf
@@ -9,7 +9,7 @@ terraform {
   required_providers {
     azurerm = {
       source  = "hashicorp/azurerm"
-      version = "=3.0.1"
+      version = "=3.4.0"
     }
   }
 }
